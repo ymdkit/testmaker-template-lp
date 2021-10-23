479e5f928fbe8d28e6fe63e368a6d5bff24f0ac6
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-10-24 00:20:28 +0900

    Add: プライバシーポリシーの追加

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -5,8 +5,16 @@ const Privacy: NextPage = () => {
   return (
     <div>
       <Layout>
-        <div className="container mx-auto">
-          <h1>プライバシーポリシー</h1>
+        <div className="container mx-auto py-3">
+          <h1 className="text-3xl">プライバシーポリシー</h1>
+          <h2 className="text-2xl mt-3">広告について</h2>
+          <p className="mt-3">
+            本サービスでは、第三者配信の広告サービス（AdMob）を利用しています。この広告事業者は、広告表示のためにお客様の情報の一部を送信する場合があります。
+          </p>
+          <p>Google 広告に関するポリシー</p>
+          <a href="https://policies.google.com/technologies/ads?hl=ja">
+            https://policies.google.com/technologies/ads?hl=ja
+          </a>
         </div>
       </Layout>
     </div>
