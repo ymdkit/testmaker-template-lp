62b6d6d285993bc0f4bb36706d5513933a0bebfd
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-10-24 01:23:50 +0900

    Fix: Lint の指摘箇所を修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,12 +9,16 @@ export default function Header() {
             <a>{process.env.NEXT_PUBLIC_APP_NAME}</a>
           </Link>
           <div className="flex-grow"></div>
-          <a href="/privacy" className="block mx-3">
-            プライバシーポリシー
-          </a>
-          <a href="/terms" className="block mx-3">
-            利用規約
-          </a>
+          <Link href="/privacy">
+            <a className="block mx-3">
+              プライバシーポリシー
+            </a>
+          </Link>
+          <Link href="/privacy">
+            <a className="block mx-3">
+              利用規約
+            </a>
+          </Link>
         </div>
       </header>
     </div>
