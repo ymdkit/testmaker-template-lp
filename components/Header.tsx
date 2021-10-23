1c5a52c057649a362ff3defcbf682f2847f41838
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-10-23 23:08:05 +0900

    Add: favicon の動的変更に対応

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import Head from 'next/head'
 
 export default function Header() {
 
   return (
     <div>
+      <Head>
+        <link rel="shortcut icon" href={`${process.env.NEXT_PUBLIC_APP_NAME_EN}/favicon.ico`}/>
+      </Head>
       <header className="bg-black">
         <div className="flex p-3">
           <Link href="/">
